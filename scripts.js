60a91a5d44eb6bcf2fb704e8ca0f6748e94b657d
Yono1112 <yuma.ohno.sss@gmail.com>
2024-06-03 14:43:37 +0900

    create delete button and done checkbox

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 const addButton = document.querySelector("button");
-const todoLists = document.querySelector("#todoLists")
-let todoInput = document.querySelector("#inputText")
+const todoLists = document.querySelector("#todoLists");
+const todoInput = document.querySelector("#inputText");
 
 addButton.addEventListener("click", () => {
 	const inputText = todoInput.value;
 	if (inputText.trim() != '') {
-		let todoList = document.createElement("li");
-		todoList.textContent = inputText;
+		const todoList = document.createElement("li");
+
+		const checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.addEventListener("change", () => {
+			if (checkbox.checked) {
+				todoList.style.textDecoration = "line-through";
+			} else {
+				todoList.style.textDecoration = "none";
+			}
+		});
+		todoList.appendChild(checkbox);
+
+		const textNode = document.createTextNode(inputText);
+		todoList.appendChild(textNode);
+
+		const removeButton = document.createElement("button");
+		removeButton.textContent = "delete";
+		removeButton.addEventListener("click", () => {
+			todoLists.removeChild(todoList);
+		});
+		todoList.appendChild(removeButton);
 
 		todoLists.appendChild(todoList);
-
 		todoInput.value = "";
 	}
 })
